47271947e408f323f01d5021ae1e664cd6c2e9df
untill99 <gabriel.kasten@outlook.com>
2025-12-07 19:08:18 -0300

    feat(weather): Comment out main execution block for weather data processing

--- pipelines/weather/pipe_weather.py
+++ pipelines/weather/pipe_weather.py
@@ -342,24 +342,24 @@ def populate_historical_weather_data_by_date_range(
 
 
 if __name__ == "__main__":
-    forecast_data = fetch_forecast_weather_data("Joinville")
-    forecast_data = format_forecast_weather_data(forecast_data)
-    delete_forecast_weather_data()
-    insert_forecast_weather_data_into_database(forecast_data)
+    # forecast_data = fetch_forecast_weather_data("Joinville")
+    # forecast_data = format_forecast_weather_data(forecast_data)
+    # delete_forecast_weather_data()
+    # insert_forecast_weather_data_into_database(forecast_data)
 
-    start_date = "2025-10-30"
-    end_date = "2025-10-30"
-    date_list = (
-        pd.date_range(start=start_date, end=end_date).strftime("%Y-%m-%d").tolist()
-    )
+    # start_date = "2025-10-30"
+    # end_date = "2025-10-30"
+    # date_list = (
+    #     pd.date_range(start=start_date, end=end_date).strftime("%Y-%m-%d").tolist()
+    # )
 
-    for date in date_list:
-        print(f"Processando data: {date}")
-        weather_data = fetch_history_weather_data("Joinville", date)
-        weather_data = format_history_weather_data(weather_data)
+    # for date in date_list:
+    #     print(f"Processando data: {date}")
+    #     weather_data = fetch_history_weather_data("Joinville", date)
+    #     weather_data = format_history_weather_data(weather_data)
 
-        print(f"{len(weather_data)} registros formatados")
+    #     print(f"{len(weather_data)} registros formatados")
 
-        # delete_history_weather_data(date)
-        insert_history_weather_data_into_database(weather_data)
-        print("Dados inseridos no banco com sucesso!")
+    #     # delete_history_weather_data(date)
+    #     insert_history_weather_data_into_database(weather_data)
+    #     print("Dados inseridos no banco com sucesso!")
